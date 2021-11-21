542935a84ffa7e971185f38cfcf648e88ccd84f8
Puru Jaiswal <purujaiswal1996@gmail.com>
2021-11-21 03:05:43 -0600

    regsiter py

--- app.py
+++ app.py
@@ -26,13 +26,12 @@ def login():
         user_name = request.form['username']
         password = request.form['password']
         cursor = mysql.get_db().cursor()
-        cursor.execute('SELECT * FROM Users WHERE UserName = %s AND Password = %s',(user_name, password))
+        cursor.execute('SELECT * FROM Users WHERE UserName = %s AND Password = %s',(user_name, password,))
         account = cursor.fetchone()
-        print(account)
         if account:
             session['loggedin'] = True
-            session['id'] = account['id']
-            session['username'] = account['username']
+            session['id'] = account[0]
+            session['username'] = account[1]
             msg = 'Logged in successfully !'
             return render_template('index.html', msg=display_message)
         else:
@@ -49,12 +48,14 @@ def logout():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     msg = ''
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and \
+            'email' in request.form and phone in request.form :
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username,))
+        phone = request.form['phone']
+        cursor = mysql.get_db().cursor()
+        cursor.execute('SELECT * FROM Users WHERE UserName = % s', (username,))
         account = cursor.fetchone()
         if account:
             msg = 'Account already exists !'
@@ -65,7 +66,7 @@ def register():
         elif not username or not password or not email:
             msg = 'Please fill out the form !'
         else:
-            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email,))
+            cursor.execute('INSERT INTO Users VALUES (NULL, % s, % s, % s, %s, %s)', (username, password, email, phone, "User",))
             mysql.connection.commit()
             msg = 'You have successfully registered !'
     elif request.method == 'POST':
