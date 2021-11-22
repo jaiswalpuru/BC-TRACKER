9b2e8c8a0515dfc9b0e024aa2b6f68f4aa00ad5f
Puru Jaiswal <purujaiswal1996@gmail.com>
2021-11-21 23:04:20 -0600

    trader separated with some more functionalities

--- app.py
+++ app.py
@@ -18,30 +18,47 @@ app.config['MYSQL_DATABASE_DB'] = config['DB']
 
 mysql = MySQL(app)
 
+#Fetch the data which need to be shown to respective user.
+def get_data(user_type):
+    if user_type == 'user':
+        return
+    elif user_type == 'admin':
+        return 'admin'
+    else :
+        cursor = mysql.get_db().cursor()
+        cursor.execute('SELECT * FROM Transaction WHERE Status = %s ', ("pending", ))
+        data  = cursor.fetchall()
+        return data
+
 @app.route("/")
 @app.route("/login", methods=['GET','POST'])
 def login():
     msg = ''
-    type = ''
+    user_type = ''
+    file_load=''
     if request.method=='POST' and 'username' in request.form and 'password' in request.form and \
             ('checkuser' in request.form or 'checkadmin' in request.form or 'checktrader' in request.form):
         user_name = request.form['username']
         password = request.form['password']
         if 'checkuser' in request.form :
-            type = 'user'
+            user_type = 'user'
+            file_load = 'index.html'
         elif 'checktrader' in request.form:
-            type = 'trader'
+            user_type = 'trader'
+            file_load = 'trader.html'
         else:
-            type = 'admin'
+            user_type = 'admin'
+            file_load = 'admin.html'
+        data = get_data(user_type);
         cursor = mysql.get_db().cursor()
-        cursor.execute('SELECT * FROM Users WHERE UserName = %s AND Password = %s and Type = %s',(user_name, password, type,))
+        cursor.execute('SELECT * FROM Users WHERE UserName = %s AND Password = %s and Type = %s ',(user_name, password, user_type,))
         account = cursor.fetchone()
         if account:
             session['loggedin'] = True
             session['id'] = account[0]
             session['username'] = account[1]
             msg = 'Logged in successfully !'
-            return render_template('index.html', msg=msg)
+            return render_template(file_load, msg=msg)
         else:
             msg = 'Incorrect username / password !'
     return render_template('login.html', msg=msg)
@@ -57,13 +74,18 @@ def logout():
 def register():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and \
-            'email' in request.form and 'phone' in request.form :
+            'email' in request.form and 'phone' in request.form and 'phone' in request.form and 'staddress' in request.form and \
+            'city' in request.form and 'zip' in request.form and 'state' in request.form:
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
         phone = request.form['phone']
+        zip = request.form['zip']
+        state = request.form['state']
+        city = request.form['city']
+        street_address = request.form['staddress']
         cursor = mysql.get_db().cursor()
-        cursor.execute('SELECT * FROM Users WHERE UserName = % s', (username,))
+        cursor.execute('SELECT * FROM USERS WHERE UserName = %s ', (username,))
         account = cursor.fetchone()
         if account:
             msg = 'Account already exists !'
@@ -74,7 +96,11 @@ def register():
         elif not username or not password or not email or not phone:
             msg = 'Please fill out the form !'
         else:
-            cursor.execute('INSERT INTO Users VALUES (NULL, % s, % s, % s, %s, %s)', (username, password, email, phone, "user",))
+            cursor.execute('INSERT INTO USERS VALUES (NULL, % s, % s, % s, %s, %s)', (username, password, phone, email, "user",))
+            cursor.execute('SELECT ClientId FROM USERS WHERE UserName = %s ', (username,))
+            client_id = cursor.fetchone()[0]
+            cursor.execute('INSERT INTO ADDRESS VALUES (%s, %s, %s, %s, %s)', (client_id, street_address, city, state, zip))
+            cursor.execute('INSERT INTO ACC_DETAILS VALUES (%s, %s, %s)', (client_id, '100000', '100000'))
             mysql.get_db().commit()
             msg = 'You have successfully registered !'
     elif request.method == 'POST':
---
+++ queries.sql
@@ -0,0 +1,76 @@
+CREATE DATABASE IF NOT EXISTS `BITCOIN_TRANSACTION` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+USE BITCOIN_TRANSACTION;
+
+-- Complete
+CREATE TABLE IF NOT EXISTS `USERS` (
+	`ClientId` int(11) NOT NULL AUTO_INCREMENT,
+    `UserName` varchar(100) NOT NULL,
+    `Password` varchar(100) NOT NULL,
+	`Phone` varchar(100) NOT NULL,
+	`Email` varchar(100) NOT NULL,
+	`Type` varchar(100) NOT NULL, -- admin, trader, user:(gold, silver)
+    PRIMARY KEY(`ClientId`)
+)Engine=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- Complete
+CREATE TABLE IF NOT EXISTS `ADDRESS` (
+	`ClientId` int(11) NOT NULL,
+    `StreetAddress` varchar(100) NOT NULL,
+    `City` varchar(100) NOT NULL,
+	`State` varchar(100) NOT NULL,
+	`ZipCode` int(11) NOT NULL,
+    PRIMARY KEY(`ClientId`),
+    FOREIGN KEY (ClientId) REFERENCES USERS(ClientId)
+)Engine=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- Complete
+CREATE TABLE IF NOT EXISTS `ACC_DETAILS` (
+	`ClientId` int(11) NOT NULL,
+    `TotalAmount` FLOAT(10) NOT NULL, -- Bticoin amt + Fiat currency
+	`FiatCurrecy` FLOAT(10) NOT NULL, -- $ amount
+    PRIMARY KEY(`ClientId`),
+    FOREIGN KEY (ClientId) REFERENCES USERS(ClientId)
+)Engine=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+Select * from Users;
+
+-- Complete
+CREATE TABLE IF NOT EXISTS `BITCOIN` (
+	`ClientId` int(11) NOT NULL,
+    `BitCoinType` varchar(100) NOT NULL,
+    `Units` int(11) NOT NULL,
+    PRIMARY KEY(`ClientId`,`BitCoinType`),
+    FOREIGN KEY (ClientId) REFERENCES USERS(ClientId)
+)Engine=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- Complete
+CREATE TABLE IF NOT EXISTS `TRANSACTION` (
+	`ClientId` int(11) NOT NULL, -- Who is initiating the transaction
+    `TransactionId` varchar(100) NOT NULL,
+    `TransactionType` varchar(10) NOT NULL, -- Buy or sell
+	`Date` DATE NOT NULL,
+	`CommisionPaid` FLOAT(10) NOT NULL,
+    `CommisionType` varchar(10) NOT NULL,
+    `RecipientId` varchar(10) NOT NULL,
+    `Status` varchar(20) NOT NULL,
+    PRIMARY KEY (`TransactionId`),
+    FOREIGN KEY (ClientId) REFERENCES USERS(ClientId)
+)Engine=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `Transaction` (`ClientId`,`TransactionId`,`TransactionType`, `Date`, `CommisionPaid`, `CommisionType`, `RecipientId`, `Status`) 
+VALUES (19, 'random1', 'Sell',DATE(), , , 20, 'pending');
+
+Select * from Address;
+Select * from Transaction;
+Select * from Users;
+SELECT * FROM Address;
+
+
+-- INSERT INTO `USERS` (`UserName`,`Password`,`Phone`, `Email`, `Type`) VALUES ('Admin', '@dm1n', '4699272570','iamadmin@gmail.com', 'admin');
+
+-- INSERT INTO `USERS` (`UserName`,`Password`,`Phone`, `Email`, `Type`) VALUES ('Trader_two', 'trader@2', '4699252577','iamtradertwo@gmail.com', 'trader');
+
+-- GRANT ALL ON BITCOIN_TRANSACTION.* TO 'root'@'localhost';
+
+
